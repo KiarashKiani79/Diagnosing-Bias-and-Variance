c75450a3bb48ee79c88b440d955de8e163f24485
Kiarash Kiani <kiarash.kiani7997@gmail.com>
2024-05-10 19:56:18 +0330

    prepare the dataset by loading and split it

--- fix_high_bias.py
+++ fix_high_bias.py
@@ -1,2 +1,11 @@
 from imports import *
 
+# Load and split the data
+x_train, y_train, x_cv, y_cv, x_test, y_test = utils.prepare_dataset('data/c2w3_lab2_data1.csv')
+# x_train.shape = (60, 1) , y_train.shape = (60, )
+# x_cv.shape = (20, 1) , y_cv.shape = (20, )
+
+# Initialize the linear-model
+model = LinearRegression()
+
+# Adding polynomial features
